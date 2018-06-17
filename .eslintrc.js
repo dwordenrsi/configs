45c9f8f413ba3cca3e9cd3fd72c85ef9dc8fd638
Don Worden <donworden@outlook.com>
2018-06-17 10:12:34 -0500

    added a project, updated .eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  root: true,
+  'root': true,
   'ecmaVersion': 6,
   'environment': {
     'es6': true,
@@ -9,13 +9,11 @@ module.exports = {
   'ecmaFeatures': {
     'arrowFunctions': true
   },
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'sourceType': 'module'
   },
-  plugins: [
-    'html', 'json'
-  ],
+  'plugins': ['html', 'json'],
   'rules': {
     // General
     'no-cond-assign': 2,
@@ -26,17 +24,19 @@ module.exports = {
     'no-empty-character-class': 2,
     'no-ex-assign': 2,
     'no-extra-boolean-cast': 2,
-    'no-extra-parens': [2, 'all', {'nestedBinaryExpressions': false}],
+    'no-extra-parens': [2, 'all', { 'nestedBinaryExpressions': false }],
     'no-extra-semi': 2,
     'no-func-assign': 2,
     'no-invalid-regexp': 2,
     'no-irregular-whitespace': [
-      2, {
+      2,
+      {
         'skipStrings': true,
         'skipComments': true,
         'skipRegExps': true,
         'skipTemplates': true
-    }],
+      }
+    ],
     'no-negated-in-lhs': 2,
     'no-obj-calls': 2,
     'no-regex-spaces': 2,
@@ -47,10 +47,12 @@ module.exports = {
     'use-isnan': 2,
     'valid-jsdoc': 0,
     'valid-typeof': 2,
+    'for-direction': 2,
     // Best Practices
     'accessor-pairs': 2,
     'array-callback-return': 2,
     'block-scoped-var': 2,
+    'class-methods-use-this': 2,
     'complexity': [2, 8],
     'consistent-return': 2,
     'curly': [2, 'multi-line'],
@@ -59,10 +61,15 @@ module.exports = {
     'dot-notation': 2,
     'eqeqeq': [2, 'smart'],
     'guard-for-in': 2,
+    'getter-return': 2,
     'no-alert': 1,
+    'no-await-in-loop': 2,
     'no-caller': 2,
     'no-case-declarations': 2,
+    'no-compare-neg-zero': 2,
     'no-div-regex': 2,
+    'no-dupe-args': 2,
+    'no-dupe-keys': 2,
     'no-else-return': 0,
     'no-empty-function': 2,
     'no-empty-pattern': 2,
@@ -76,10 +83,13 @@ module.exports = {
     'no-labels': 2,
     'no-lone-blocks': 2,
     'no-loop-func': 2,
-    'no-magic-numbers': [1, {'ignoreArrayIndexes': true, 'ignore': [0, 1, 403, 500]}],
+    'no-magic-numbers': [
+      1,
+      { 'ignoreArrayIndexes': true, 'ignore': [0, 1, 403, 500] }
+    ],
     'no-multi-spaces': 2,
     'no-multi-str': 2,
-    'no-native-reassign': [2, {'exceptions': ['Object']}],
+    'no-native-reassign': [2, { 'exceptions': ['Object'] }],
     'no-new': 2,
     'no-new-func': 2,
     'no-new-wrappers': 2,
@@ -94,7 +104,10 @@ module.exports = {
     'no-sequences': 2,
     'no-throw-literal': 2,
     'no-unmodified-loop-condition': 2,
-    'no-unused-expressions': [2, {'allowShortCircuit': true, 'allowTernary': true}],
+    'no-unused-expressions': [
+      2,
+      { 'allowShortCircuit': true, 'allowTernary': true }
+    ],
     'no-useless-call': 2,
     'no-useless-concat': 2,
     'no-useless-escape': 2,
@@ -108,9 +121,9 @@ module.exports = {
     // Style
     'array-bracket-spacing': [2, 'never'],
     'block-spacing': [2, 'never'],
-    'brace-style': [2, '1tbs', {'allowSingleLine': true}],
+    'brace-style': [2, '1tbs', { 'allowSingleLine': true }],
     'camelcase': 2,
-    'comma-spacing': [2, {'before': false, 'after': true }],
+    'comma-spacing': [2, { 'before': false, 'after': true }],
     'comma-style': [2, 'last'],
     'comma-dangle': 2,
     'computed-property-spacing': [2, 'never'],
@@ -118,8 +131,11 @@ module.exports = {
     'eol-last': 2,
     'func-names': 0,
     'func-style': [2, 'declaration', { 'allowArrowFunctions': true }],
-    'indent': [2, 2, {SwitchCase: 1}],
-    'key-spacing': [2, { 'beforeColon': false, 'afterColon': true, 'mode': 'strict' }],
+    'indent': [2, 2, { 'SwitchCase': 1 }],
+    'key-spacing': [
+      2,
+      { 'beforeColon': false, 'afterColon': true, 'mode': 'strict' }
+    ],
     'keyword-spacing': [2, { 'before': true }],
     'linebreak-style': [2, 'unix'],
     'max-len': [2, 120],
@@ -129,7 +145,7 @@ module.exports = {
     'no-continue': 2,
     'no-lonely-if': 2,
     'no-mixed-spaces-and-tabs': [2, 'smart-tabs'],
-    'no-multiple-empty-lines': [2, {'max': 2}],
+    'no-multiple-empty-lines': [2, { max: 2 }],
     'no-negated-condition': 2,
     'no-new-object': 2,
     'no-spaced-func': 2,
@@ -140,19 +156,19 @@ module.exports = {
     'padded-blocks': [2, 'never'],
     'quotes': [2, 'single'],
     'semi': [2, 'always'],
-    'semi-spacing': [2, {'before': false, 'after': true}],
+    'semi-spacing': [2, { 'before': false, 'after': true }],
     'sort-vars': [2, { 'ignoreCase': false }],
     'space-before-blocks': 2,
     'space-before-function-paren': [2, 'never'],
     'space-in-parens': [2, 'never'],
-    'space-infix-ops': [2, {'int32Hint': false}],
+    'space-infix-ops': [2, { 'int32Hint': false }],
     'spaced-comment': [2, 'always'],
     'wrap-regex': 2,
     // ES6
     'arrow-body-style': [1, 'as-needed'],
     'arrow-parens': [2, 'as-needed'],
-    'arrow-spacing': [2, {'before': true, 'after': true}],
-    'generator-star-spacing': [2, {'before': true, 'after': false}],
+    'arrow-spacing': [2, { 'before': true, 'after': true }],
+    'generator-star-spacing': [2, { 'before': true, 'after': false }],
     'no-const-assign': 2,
     'no-dupe-class-members': 2,
     'no-duplicate-imports': [2, { 'includeExports': true }],
@@ -162,6 +178,6 @@ module.exports = {
     'prefer-arrow-callback': 2,
     'prefer-const': 2,
     'require-yield': 2,
-    'yield-star-spacing': [2, {'before': true, 'after': false}]
+    'yield-star-spacing': [2, { 'before': true, 'after': false }]
   }
-}
+};
